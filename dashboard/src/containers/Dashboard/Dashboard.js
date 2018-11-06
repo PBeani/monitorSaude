29a9f859710862b0a187bfdacc5195b07cc77c4d
Carla Vieira <carla.piazzon.vieira@usp.br>
2018-11-06 09:46:53 -0200

    criação do component de dashboard da home

--- dashboard/src/containers/Dashboard/Dashboard.js
+++ dashboard/src/containers/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TableauDashboard from '../../components/TableauDashboard/TableauDashboard';
 
 class Dashboard extends Component {
     state = {};
@@ -7,6 +8,7 @@ class Dashboard extends Component {
         return(
             <div>
                 <h1>Pagina principal</h1>
+                <TableauDashboard path={this.props.match.path} />
             </div>
         )
     }
